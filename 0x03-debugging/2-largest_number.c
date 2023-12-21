58da634a2ba2df88f3ac12113fca938d58196af7
Tshwene Vincent Baloyi <t.v8aloyi@gmail.com>
2023-12-20 23:19:19 -0800

    fixed 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,17 +10,14 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+    int largest = a;
 
-    if (a >= b && a >= c)
-    {
-        largest = a;
-    }
-    else if (b >= a && b >= c)
+    if (b > largest)
     {
         largest = b;
     }
-    else
+
+    if (c > largest)
     {
         largest = c;
     }
